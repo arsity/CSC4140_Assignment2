82b187e4c62abbcd067c5cb1a6abcbf1ebe2e0ba
Luke <43494998+arsity@users.noreply.github.com>
2023-01-26 17:17:57 +0800

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,144 @@
+#include "../headers/Triangle.hpp"
+#include "../headers/rasterizer.hpp"
+#include <eigen3/Eigen/Eigen>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <cmath>
+// add some other header files you need
+
+constexpr double MY_PI = 3.1415926;
+
+Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
+{
+    Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
+
+    Eigen::Matrix4f translate;
+    translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1,
+        -eye_pos[2], 0, 0, 0, 1;
+
+    view = translate * view;
+    // std::clog << "view" << std::endl << view << std::endl;  // check data
+
+    return view;
+}
+
+
+Eigen::Matrix4f get_model_matrix(float rotation_angle, Eigen::Vector3f T, Eigen::Vector3f S, Eigen::Vector3f P0, Eigen::Vector3f P1)
+{
+
+    //Step 1: Build the Translation Matrix M_trans:
+
+    //Step 2: Build the Scale Matrix S_trans:
+
+    //Step 3: Implement Rodrigues' Rotation Formular, rotation by angle theta around axix u, then get the model matrix
+	// The axis u is determined by two points, u = P1-P0: Eigen::Vector3f P0 ,Eigen::Vector3f P1  
+    // Create the model matrix for rotating the triangle around a given axis. // Hint: normalize axis first
+
+	//Step 4: Use Eigen's "AngleAxisf" to verify your Rotation
+	//Eigen::AngleAxisf rotation_vector(radian, Vector3f(axis[0], axis[1], axis[2]));  
+	//Eigen::Matrix3f rotation_matrix;
+	//rotation_m = rotation_vector.toRotationMatrix();
+
+	return model;
+}
+
+
+
+Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
+                                      float zNear, float zFar)
+{
+    // Implement this function
+
+
+    // TODO: Implement this function
+    // Create the projection matrix for the given parameters.
+    // Then return it.
+
+    // frustum -> cubic
+
+    // orthographic projection
+
+    // squash all transformations
+
+    // std::clog << "projection" << std::endl << projection << std::endl; //check
+
+    return projection;
+}
+
+int main(int argc, const char** argv)
+{
+    float angle = 0;
+    bool command_line = false;
+    std::string filename = "result.png";
+
+    if (argc >= 3) {
+        command_line = true;
+        angle = std::stof(argv[2]); // -r by default
+        if (argc == 4) {
+            filename = std::string(argv[3]);
+        }
+        else
+            return 0;
+    }
+
+    rst::rasterizer r(1024, 1024);
+    // define your eye position "eye_pos" to a proper position
+
+    // define a triangle named by "pos" and "ind"
+
+
+    auto pos_id = r.load_positions(pos);
+    auto ind_id = r.load_indices(ind);
+
+    int key = 0;
+    int frame_count = 0;
+
+    // added parameters for get_projection_matrix(float eye_fov, float aspect_ratio,float zNear, float zFar)
+    /
+    // Eigen::Vector3f axis(0, 0, 1);
+    Eigen::Vector3f axis(1, 0, 0);
+
+    if (command_line) {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        r.set_model(get_model_matrix(angle, T, S, P0, P1));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(eye_fov, aspect_ratio, zNear, zFar));
+
+        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+
+        cv::imwrite(filename, image);
+
+        return 0;
+    }
+
+    while (key != 27) {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        r.set_model(get_model_matrix(angle));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(eye_fov, aspect_ratio, zNear, zFar));
+
+        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+
+        cv::Mat image(1024, 1024, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+        cv::imshow("image", image);
+        key = cv::waitKey(10);
+
+        std::cout << "frame count: " << frame_count++ << '\n';
+        std::clog << "angle: " << angle << std::endl;
+    
+
+        if (key == 'a') {
+            angle += 10;
+        }
+        else if (key == 'd') {
+            angle -= 10;
+        }
+    }
+
+    return 0;
+}
