f2be00bf25ed2189b66b050f5c37e352cc279d4d
Luke <43494998+arsity@users.noreply.github.com>
2023-02-14 22:20:03 +0800

    finish

--- scratch.sh
+++ scratch.sh
@@ -5,4 +5,4 @@ mkdir "build" || rm -rf "build" && mkdir "build"
 
 cmake -DCMAKE_BUILD_TYPE=Release -B "build"
 cmake --build "build"
-./build/CSC4140_Assignment2 "$1" "$2" "../$3"
+./build/CSC4140_Assignment2 "$@"
