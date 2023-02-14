4efdcba8142005615714033fc77f9de9874dbacc
Luke <43494998+arsity@users.noreply.github.com>
2023-02-14 19:27:27 +0800

    rough finish

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 constexpr double MY_PI = 3.1415926;
 
-Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos) {
+Eigen::Matrix4f get_view_matrix(const Eigen::Vector3f &eye_pos) {
     Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
 
     Eigen::Matrix4f translate;
@@ -22,10 +22,10 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos) {
 }
 
 Eigen::Matrix4f get_model_matrix(float rotation_angle,
-                                 Eigen::Vector3f T,
-                                 Eigen::Vector3f S,
-                                 Eigen::Vector3f P0,
-                                 Eigen::Vector3f P1) {
+                                 const Eigen::Vector3f &T = {0, 0, 0},
+                                 const Eigen::Vector3f &S = {1, 1, 1},
+                                 const Eigen::Vector3f &P0 = {0, 0, 0},
+                                 const Eigen::Vector3f &P1 = {0, 0, -1}) {
 
     //Step 1: Build the Translation Matrix M_trans:
 
@@ -56,12 +56,12 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle,
     Eigen::Vector3f u{P1 - P0};
     u.normalize();
     Eigen::Matrix3f lastMatrix = Eigen::Matrix3f::Zero();
-    lastMatrix(0, 1) = -u[2];
-    lastMatrix(1, 0) = u[2];
-    lastMatrix(0, 2) = u[1];
-    lastMatrix(2, 0) = -u[1];
-    lastMatrix(1, 2) = u[0];
-    lastMatrix(2, 1) = -u[0];
+    lastMatrix(0, 1) = -u(2);
+    lastMatrix(1, 0) = u(2);
+    lastMatrix(0, 2) = u(1);
+    lastMatrix(2, 0) = -u(1);
+    lastMatrix(1, 2) = u(0);
+    lastMatrix(2, 1) = -u(0);
 
     Eigen::Matrix3f
         modelMatrix = cos(rotation_angle) * Eigen::Matrix3f::Identity()
@@ -69,18 +69,30 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle,
         + sin(rotation_angle) * lastMatrix;
 
     Eigen::Matrix4f model_Matrix = Eigen::Matrix4f::Identity();
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
+    for (size_t i = 0; i < 3; ++i) {
+        for (size_t j = 0; j < 3; ++j) {
             model_Matrix(i, j) = modelMatrix(i, j);
         }
     }
 
     // Step 4 Implement
-    Eigen::AngleAxisf rotation_vector(rotation_angle, Vector3f(u[0], u[1], u[2]));
+    Eigen::AngleAxisf rotation_vector(rotation_angle, Vector3f(u(0), u(1), u(2)));
     Eigen::Matrix3f rotation_matrix = rotation_vector.toRotationMatrix();
     eigen_assert(rotation_matrix == modelMatrix);
 
     return S_trans * model_Matrix * M_trans;
+}
+
+// helper function for projection matrix
+Eigen::Matrix4f get_orthogonal_matrix(float l, float r, float b, float t, float n, float f) {
+    Eigen::Matrix4f res = Eigen::Matrix4f::Identity();
+    res(0, 0) = float(2) / (r - l);
+    res(1, 1) = float(2) / (t - b);
+    res(2, 2) = float(2) / (n - f);
+    res(0, 3) = -(r + l) / (r - l);
+    res(1, 3) = -(t + b) / (t - b);
+    res(2, 3) = -(n + f) / (n - f);
+    return res;
 }
 
 Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
@@ -100,7 +112,22 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
 
     // std::clog << "projection" << std::endl << projection << std::endl; //check
 
+    Eigen::Matrix4f M_persp = Eigen::Matrix4f::Zero();
+    M_persp(0, 0) = zNear;
+    M_persp(1, 1) = zNear;
+    M_persp(3, 2) = 1;
+    M_persp(2, 2) = zNear + zFar;
+    M_persp(2, 3) = -zNear * zFar;
 
+    auto angle = eye_fov / 2 / 180 * MY_PI;
+    auto h = -zNear * tan(angle);
+    auto w = h * aspect_ratio;
+    auto M_ortho = get_orthogonal_matrix(-w, w, -h, h, zNear, zFar);
+
+    Eigen::Matrix4f projection = M_ortho * M_persp;
+
+    // debug line
+    std::clog << "projection" << std::endl << projection << std::endl;
 
     return projection;
 }
@@ -115,15 +142,23 @@ int main(int argc, const char** argv) {
         angle = std::stof(argv[2]); // -r by default
         if (argc == 4) {
             filename = std::string(argv[3]);
-        } else
+        } else {
+            std::cerr << "Invalid Argument!" << std::endl << std::endl;
+            std::cout << "Help:" << std::endl;
+            std::cout << "\t" << "[-r <degree> [<saving path>]]" << std::endl;
             return 0;
+        }
     }
 
     rst::rasterizer r(1024, 1024);
     // define your eye position "eye_pos" to a proper position
+    const Eigen::Vector3f eye_pos{0, 0, 10};
 
     // define a triangle named by "pos" and "ind"
 
+    std::vector<Eigen::Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}};
+
+    std::vector<Eigen::Vector3i> ind{{0, 1, 2}};
 
     auto pos_id = r.load_positions(pos);
     auto ind_id = r.load_indices(ind);
@@ -132,13 +167,21 @@ int main(int argc, const char** argv) {
     int frame_count = 0;
 
     // added parameters for get_projection_matrix(float eye_fov, float aspect_ratio,float zNear, float zFar)
-    /
+    constexpr float eye_fov = 60; // use degree notation
+    constexpr float aspect_ratio = 1; // use cubic
+    constexpr float zNear = 20;
+    constexpr float zFar = 80;
+
+    Eigen::Vector3f T = Eigen::Vector3f::Zero();
+    Eigen::Vector3f S = Eigen::Vector3f::Ones();
+    Eigen::Vector3f P0{0, 0, 0};
+    Eigen::Vector3f P1{1, 1, 1};
+
     // Eigen::Vector3f axis(0, 0, 1);
     Eigen::Vector3f axis(1, 0, 0);
 
     if (command_line) {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
-
         r.set_model(get_model_matrix(angle, T, S, P0, P1));
         r.set_view(get_view_matrix(eye_pos));
         r.set_projection(get_projection_matrix(eye_fov, aspect_ratio, zNear, zFar));
@@ -154,8 +197,7 @@ int main(int argc, const char** argv) {
 
     while (key != 27) {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
-
-        r.set_model(get_model_matrix(angle));
+        r.set_model(get_model_matrix(angle, T, S, P0, P1));
         r.set_view(get_view_matrix(eye_pos));
         r.set_projection(get_projection_matrix(eye_fov, aspect_ratio, zNear, zFar));
 
